a73c1aa11b7f5ff386b4ec35ef343602065cef86
Lacey Savage <lacey.savage@gmail.com>
2020-11-23 16:26:05 -0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,407 @@
+    //This is the <main> structure for it 
+    //     <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+
+    // <style>
+    // //     article,
+    // //     aside,
+    // //     details,
+    // //     figcaption,
+    // //     figure,
+    // //     footer,
+    // //     header,
+    // //     hgroup,
+    // //     nav,
+    // //     section,
+    //     summary {
+    //         display: block;
+    //     }
+    
+    //     audio,
+    //     canvas,
+    //     video {
+    //         display: inline-block;
+    //     }
+    
+    //     audio:not([controls]) {
+    //         display: none;
+    //         height: 0;
+    //     }
+    
+    //     [hidden] {
+    //         display: none;
+    //     }
+    
+    //     html {
+    //         font-family: sans-serif;
+    //         -webkit-text-size-adjust: 100%;
+    //         -ms-text-size-adjust: 100%;
+    //     }
+    
+    //     a:focus {
+    //         outline: thin dotted;
+    //     }
+    
+    //     a:active,
+    //     a:hover {
+    //         outline: 0;
+    //     }
+    
+    //     h1 {
+    //         font-size: 2em;
+    //     }
+    
+    //     abbr[title] {
+    //         border-bottom: 1px dotted;
+    //     }
+    
+    //     b,
+    //     strong {
+    //         font-weight: 700;
+    //     }
+    
+    //     dfn {
+    //         font-style: italic;
+    //     }
+    
+    //     mark {
+    //         background: #ff0;
+    //         color: #000;
+    //     }
+    
+    //     code,
+    //     kbd,
+    //     pre,
+    //     samp {
+    //         font-family: monospace, serif;
+    //         font-size: 1em;
+    //     }
+    
+    //     pre {
+    //         white-space: pre-wrap;
+    //         word-wrap: break-word;
+    //     }
+    
+    //     q {
+    //         quotes: \201C \201D \2018 \2019;
+    //     }
+    
+    //     small {
+    //         font-size: 80%;
+    //     }
+    
+    //     sub,
+    //     sup {
+    //         font-size: 75%;
+    //         line-height: 0;
+    //         position: relative;
+    //         vertical-align: baseline;
+    //     }
+    
+    //     sup {
+    //         top: -.5em;
+    //     }
+    
+    //     sub {
+    //         bottom: -.25em;
+    //     }
+    
+    //     img {
+    //         border: 0;
+    //     }
+    
+    //     svg:not(:root) {
+    //         overflow: hidden;
+    //     }
+    
+    //     fieldset {
+    //         border: 1px solid silver;
+    //         margin: 0 2px;
+    //         padding: .35em .625em .75em;
+    //     }
+    
+    //     button,
+    //     input,
+    //     select,
+    //     textarea {
+    //         font-family: inherit;
+    //         font-size: 100%;
+    //         margin: 0;
+    //     }
+    
+    //     button,
+    //     input {
+    //         line-height: normal;
+    //     }
+    
+    //     button,
+    //     html input[type=button],
+    //     /* 1 */
+    //     input[type=reset],
+    //     input[type=submit] {
+    //         -webkit-appearance: button;
+    //         cursor: pointer;
+    //     }
+    
+    //     button[disabled],
+    //     input[disabled] {
+    //         cursor: default;
+    //     }
+    
+    //     input[type=checkbox],
+    //     input[type=radio] {
+    //         box-sizing: border-box;
+    //         padding: 0;
+    //     }
+    
+    //     input[type=search] {
+    //         -webkit-appearance: textfield;
+    //         -moz-box-sizing: content-box;
+    //         -webkit-box-sizing: content-box;
+    //         box-sizing: content-box;
+    //     }
+    
+    //     input[type=search]::-webkit-search-cancel-button,
+    //     input[type=search]::-webkit-search-decoration {
+    //         -webkit-appearance: none;
+    //     }
+    
+    //     textarea {
+    //         overflow: auto;
+    //         vertical-align: top;
+    //     }
+    
+    //     table {
+    //         border-collapse: collapse;
+    //         border-spacing: 0;
+    //     }
+    
+    //     body,
+    //     figure {
+    //         margin: 0;
+    //     }
+    
+    //     legend,
+    //     button::-moz-focus-inner,
+    //     input::-moz-focus-inner {
+    //         border: 0;
+    //         padding: 0;
+    //     }
+    
+    //     * {
+    //         box-sizing: border-box;
+    //     }
+    
+    //     body {
+    //         font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    //     }
+    
+    //     h1 {
+    //         font-weight: 500;
+    //         line-height: 1.1;
+    //     }
+    
+    //     button,
+    //     input,
+    //     select[multiple],
+    //     textarea {
+    //         background-image: none;
+    //     }
+    
+    //     input,
+    //     button,
+    //     select,
+    //     textarea {
+    //         font-family: inherit;
+    //         font-size: inherit;
+    //         line-height: inherit;
+    //     }
+    
+    //     button,
+    //     input {
+    //         line-height: normal;
+    //     }
+    
+    //     button,
+    //     input,
+    //     select,
+    //     textarea {
+    //         margin: 0;
+    //         font-family: inherit;
+    //         font-size: 100%;
+    //     }
+    
+    //     ul,
+    //     ol {
+    //         margin-top: 0;
+    //         margin-bottom: 10px;
+    //     }
+    
+    //     ul li {
+    //         cursor: move;
+    //     }
+    
+    //     a {
+    //         color: #428bca;
+    //         text-decoration: none;
+    //     }
+    
+    //     main {
+    //         padding: 30px;
+    //         margin-bottom: 30px;
+    //         font-size: 21px;
+    //         font-weight: 200;
+    //         line-height: 2.1;
+    //         color: inherit;
+    //         background-color: #eee;
+    //     }
+    
+    //     main h1 {
+    //         font-size: 63px;
+    //         line-height: 1;
+    //         color: inherit;
+    //     }
+    
+    //     .container {
+    //         max-width: 750px;
+    //         padding: 0 15px;
+    //         margin: auto;
+    //     }
+    
+    //     .page-header {
+    //         padding-bottom: 10px;
+    //         margin: 40px 0 20px;
+    //         border-bottom: 1px solid #eee;
+    //     }
+    
+    //     input {
+    //         height: 45px;
+    //         padding: 10px 16px;
+    //         font-size: 18px;
+    //         line-height: 1.3;
+    //         border-radius: 6px;
+    //     }
+    
+    //     .form-control {
+    //         display: block;
+    //         width: 100%;
+    //         height: 35px;
+    //         padding: 6px 12px;
+    //         font-size: 14px;
+    //         line-height: 1.4;
+    //         color: #555;
+    //         background-color: #fff;
+    //         border: 1px solid #ccc;
+    //         border-radius: 4px;
+    //         box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+    //         transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+    //     }
+    
+    //     .alert {
+    //         color: #b94a48;
+    //         background-color: #f2dede;
+    //         border: 1px solid #eed3d7;
+    //         padding: 15px;
+    //         margin-bottom: 20px;
+    //         border-radius: 4px;
+    //     }
+    
+    //     .text-muted {
+    //         color: #999;
+    //     }
+    // </style>
+    // <div class="container">
+    
+    //         <div class="page-header">
+    //             <h1>Lacey & Uri's Wishlist</h1>
+    //         </div>
+    
+    //         <form>
+    //             <input class="form-control" type="text" placeholder="Add an item...">
+    //         </form>
+    
+    //         <ul>
+    //             <!-- To do items get added dynamically here -->
+    //         </ul>
+    //     </div>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+
+    <script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js"></script>
+
+    <script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-database.js"></script>
+
+    <script>
+        // Your web app's Firebase configuration
+        const firebaseConfig = {
+            apiKey: "AIzaSyB_aKTzJ8aS7ma1O79UEBFra38zxuD3H78",
+            authDomain: "wishlist-4a84c.firebaseapp.com",
+            databaseURL: "https://wishlist-4a84c.firebaseio.com",
+            projectId: "wishlist-4a84c",
+            storageBucket: "wishlist-4a84c.appspot.com",
+            messagingSenderId: "381494132464",
+            appId: "1:381494132464:web:ebf51df5420070b4654c35"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+
+        $(document).ready(function () {
+
+        //create a reference to my database and save it in a variable
+        const dbRef = firebase.database().ref();
+        $('form').on('submit', function (e) {
+            e.preventDefault();
+            dbRef.on('value', function (snapshot) {
+                //code we want to execute when database changes occur: 
+                const data = snapshot.val();
+                console.log(data);
+                $('ul').empty();
+
+                //display to dos on the page
+                for (key in data) {
+                    console.log(data[key].description);
+                    $('ul').append("<li> <span class='fa fa-square-o'></span> " + data[key].description + "</li>");
+
+                }
+            });
+            if ($('input').val() !== '') {
+                // store what the user typed in a variable
+                const toDoItem = $('input').val();
+                //create an object to save in the database. We could have also just saved directly into the database
+                const toDo = {
+                    description: toDoItem,
+                    complete: false,
+                };
+                //save toDo object to database
+                dbRef.push(toDo);
+
+                // clear input
+                $('input').val('');
+            };
+        });
+
+        $('ul').on('click', 'li', function () {
+            const checkBox = $(this).find('.fa');
+            checkBox.toggleClass("fa-square-o fa-check-square-o");
+            $(this).toggleClass("text-muted")
+
+            //Method One:
+            //$(this).attr('data-key');
+
+            //Method Two:
+            //Getting information from the DOM about the element clicked on:
+            const selectedKey = $(this).data('key');
+            const toDoItemRef = firebase.database().ref(selectedKey);
+            //Getvalue ONCE from database:
+            toDoItemRef.once('value', function (data) {
+                const toDoFromDatabase = data.val();
+                console.log(toDoFromDatabase);
+                //set or //update
+                //flip complete boolean to be opposite
+                toDoItemRef.update({
+                    complete: !toDoFromDatabase.complete,
+                })
+            })
+        });
+
+    });
+
+    </script>
